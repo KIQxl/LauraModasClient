4ace6470add3588639381887f6d2c0db82741201
unknown <kaique_rba@yahoo.com.br>
2023-09-08 12:42:15 -0300

    LauraModasm v2

---
+++ JS/open-modal.js
@@ -0,0 +1,15 @@
+const openModal = document.querySelector("#btn-add");
+const modal = document.querySelector("#add-modal");
+const closeModal = document.querySelector("#btn-cancel");
+
+openModal.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    modal.showModal();
+});
+
+closeModal.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    modal.close();
+});
